405e2bccfc1b4de721583e177fecceb55ba6cac7
Tendai D. Zhou <tendaidzhou@gmail.com>
2021-03-13 02:17:07 +0200

    First commit

---
+++ src/main/java/com/dsw/formoneplaceallocator/FormOnePlaceAllocatorFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.dsw.formoneplaceallocator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormOnePlaceAllocatorFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormOnePlaceAllocatorFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dsw/formoneplaceallocator/FormOnePlaceAllocatorFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dsw.formoneplaceallocator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormOnePlaceAllocatorFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
